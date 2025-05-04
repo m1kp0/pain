e6e9b9e23ea58b629dc382a48ecf4c33934f9814
m1kpe0 <yarikasik@inbox.ru>
2025-05-04 04:45:21 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,6 +3,7 @@ print("Loading..")
 -- service
 local others = game:GetService("Players")
 local rs = game:GetService("RunService")
+local debris = game:GetService("Debris")
 local me = others.LocalPlayer
 local char, hum, hrp, remote
 
@@ -10,10 +11,14 @@ local char, hum, hrp, remote
 local l = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/turtle"))()
 
 -- toggles
+local invis_explosion_en = false
+local anti_decoy_c4_en = false
 local minigun_aura_en = false
-local minigun_aura_conn = false
 local anti_death_en = false
 local dash_anim_en = false
+local anti_decoy_c4_conn = nil
+local minigun_aura_conn = nil
+
 
 -- setting
 local radius = 60
@@ -52,14 +57,58 @@ local function minigun_aura()
     task.wait()
 end
 
+local function anti_decoy_c4_aura()
+    if me.Character ~= nil then
+        pcall(function()
+            hrp = me.Character.HumanoidRootPart
+            local mine
+            for i, mine in pairs(workspace:GetChildren()) do
+                if mine.Name == "C4" or mine.Name == "Decoy" then
+                    local mine_pos = mine.Handle.Position
+                    if (mine_pos - hrp.Position).Magnitude <= 30 then
+                        for i, v in pairs(mine:GetDescendants()) do
+                            if v:IsA("Part") or v:IsA("MeshPart") then
+                                v.Massless = true
+                            end
+                        end
+                        for i = 1, 20 do
+                            mine.Handle.CFrame = mine.Handle.CFrame * CFrame.new(1, 5, 1)
+                        end
+                    end
+                end
+            end
+        end)
+    else
+        return
+    end
+    task.wait()
+end
+
 -- gui
-local w = l:Window("Pain")
+local w = l:Window("Attack")
+local w2 = l:Window("Defense")
+local w3 = l:Window("Fun")
 
 w:Button("Get minigun", function()
     me.PlayerGui.Select.Frame.RemoteEvent:FireServer("Minigun")
 end)
 
-w:Toggle("Anti death", false, function(e) 
+w:Toggle("Minigun aura", false, function(e) 
+    minigun_aura_en = e
+    if minigun_aura_en then
+        minigun_aura_conn = rs.Heartbeat:Connect(function()
+            minigun_aura()
+        end)
+    elseif minigun_aura_conn then
+        minigun_aura_conn:Disconnect()
+    end
+end)
+
+w:Slider("Aura radius",0,500,60, function(value)
+    radius = value
+end)
+
+w2:Toggle("Anti death", false, function(e) 
     anti_death_en = e
     if anti_death_en then
         if me.Character ~= nil and anti_death_en then
@@ -84,18 +133,30 @@ w:Toggle("Anti death", false, function(e)
     end
 end)
 
-w:Toggle("Minigun aura", false, function(e) 
-    minigun_aura_en = e
-    if minigun_aura_en then
-        minigun_aura_conn = rs.Heartbeat:Connect(function()
-            minigun_aura()
+w2:Toggle("TP c4-decoy away laggy", false, function(e) 
+    anti_decoy_c4_en = e
+    if anti_decoy_c4_en then
+        anti_decoy_c4_conn = rs.RenderStepped:Connect(function()
+            anti_decoy_c4_aura()
         end)
-    elseif minigun_aura_conn then
-        minigun_aura_conn:Disconnect()
+    elseif anti_decoy_c4_conn then
+        anti_decoy_c4_conn:Disconnect()
     end
 end)
 
-w:Toggle("Dash animation", false, function(e) 
+w2:Toggle("Invis explosion", false, function(e) 
+    invis_explosion_en = e
+    workspace.ChildAdded:Connect(function(child)
+        if child.Name == "Explosion" and invis_explosion_en then
+            child.BlastRadius = 0
+            child.BlastPressure = 0
+            child.TimeScale = 0
+            child.Visible = false
+        end
+    end)
+end)
+
+w3:Toggle("Dash animation", false, function(e) 
     dash_anim_en = e
     if me.Character ~= nil and dash_anim_en then
         me.Character.Humanoid.WalkSpeed = 40
@@ -113,8 +174,6 @@ w:Toggle("Dash animation", false, function(e)
     end
 end)
 
-w:Slider("Aura radius",0,500,60, function(value)
-    radius = value
-end)
+
 
 print("Loaded!")
