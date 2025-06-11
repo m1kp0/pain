7998c8a8128cbd950a9949386cae1fca00192f33
m1kpe0 <yarikasik@inbox.ru>
2025-06-11 20:48:42 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -13,37 +13,35 @@ print("Loading..")
 -- toggles
     local invis_explosion_en = false
     local anti_decoy_c4_en = false
-    local minigun_aura_en = false
     local anti_death_en = false
     local dash_anim_en = false
+    local whitelist_en = false
+    local aura_en = false
 
 -- connections
     local anti_decoy_c4_conn = nil
-    local minigun_aura_conn = nil
+    local aura_conn = nil
 
 -- setting
     local radius = 60
 
 -- functions
-    local function minigun_aura()
-        if me.Character ~= nil then
-            pcall(function()
-                hrp = me.Character.HumanoidRootPart
-                local minigun
-                if me.Character:FindFirstChild("Minigun") then minigun = workspace:FindFirstChild(me.Name).Minigun else return end
-                if minigun then remote = minigun.RemoteFunction else return end
-                for i, other in pairs(others:GetPlayers()) do
-                    if other ~= me and other.Character ~= nil then
-                        local p_hrp = other.Character.HumanoidRootPart
-                        if (p_hrp.Position - hrp.Position).Magnitude <= radius and p_hrp.Position.Y < 500 then
-                            if other.Character.Humanoid.Health ~= 0 and not other.Character:FindFirstChild("ForceField") then
-                                remote:InvokeServer(p_hrp.Position + p_hrp.Velocity/5); task.wait()
-                            end
+    local function aura(waepon)
+        if me.Character ~= nil then pcall(function()
+            hrp = me.Character.HumanoidRootPart; local wpn
+            if me.Character:FindFirstChild(waepon) then wpn = workspace:FindFirstChild(me.Name):FindFirstChild(waepon) else return end
+            if wpn then remote = wpn.RemoteFunction else return end
+            for i, other in pairs(others:GetPlayers()) do
+                if (other ~= me and other.Character ~= nil) and not whitelist_en or not me:IsFriendsWith(other.UserId) then
+                    local p_hrp = other.Character.HumanoidRootPart
+                    if (p_hrp.Position - hrp.Position).Magnitude <= radius and p_hrp.Position.Y < 500 then
+                        if other.Character.Humanoid.Health ~= 0 or not other.Character:FindFirstChild("ForceField") then
+                            remote:InvokeServer(p_hrp.Position + p_hrp.Velocity/4); task.wait()
                         end
                     end
                 end
-            end)
-        else return end; task.wait()
+            end
+        end); else return end; task.wait()
     end
 
 -- gui
@@ -56,38 +54,43 @@ print("Loading..")
         me.PlayerGui.Select.Frame.RemoteEvent:FireServer("Spear")
     end)
 
-    w:Toggle("Minigun aura", false, function(e) 
-        minigun_aura_en = e; if minigun_aura_en then minigun_aura_conn = rs.Heartbeat:Connect(function() minigun_aura() end)
-        elseif minigun_aura_conn then minigun_aura_conn:Disconnect() end
+    w:Button("Get launcher", function()
+        me.PlayerGui.Select.Frame.RemoteEvent:FireServer("Rocket Launcher"); task.wait(.1)
+        me.PlayerGui.Select.Frame.RemoteEvent:FireServer("Spear")
     end)
+
+    w:Toggle("Minigun aura", false, function(bool) 
+        aura_en = bool; if aura_en then aura_conn = rs.Heartbeat:Connect(function() aura("Minigun") end)
+        elseif aura_conn then aura_conn:Disconnect() end
+    end)
+
+    w:Toggle("Launcher aura", false, function(bool) 
+        aura_en = bool; if aura_en then aura_conn = rs.Heartbeat:Connect(function() aura("Rocket Launcher") end) 
+        elseif aura_conn then aura_conn:Disconnect() end
+    end)
+
+    w:Toggle("Whitelist friends", false, function(bool) whitelist_en = bool end)
 
     w:Slider("Aura radius",0,500,60, function(value) radius = value end)
 
-    w2:Toggle("Anti death", false, function(e) 
-        anti_death_en = e; if anti_death_en then
+    w2:Toggle("Anti death", false, function(bool) 
+        anti_death_en = bool; if anti_death_en then
             if me.Character ~= nil and anti_death_en then
                 hrp = me.Character.HumanoidRootPart
                 local old_pos = hrp.Position.Y
-                while anti_death_en do
-                    pcall(function()
-                        hrp.CFrame = CFrame.new(hrp.Position.X, 100, hrp.Position.Z + 50)
-                        task.wait(0.1)
-                        hrp.CFrame = CFrame.new(hrp.Position.X + 50, 100, hrp.Position.Z)
-                        task.wait(0.1)
-                        hrp.CFrame = CFrame.new(hrp.Position.X, 50, hrp.Position.Z)
-                        task.wait(0.1)
-                        hrp.CFrame = CFrame.new(hrp.Position.X - 50, 50, hrp.Position.Z)
-                        task.wait(0.1)
-                        hrp.CFrame = CFrame.new(hrp.Position.X, old_pos, hrp.Position.Z - 50)
-                        task.wait()
-                    end); task.wait()
-                end
+                while anti_death_en do pcall(function()
+                    hrp.CFrame = CFrame.new(hrp.Position.X, 100, hrp.Position.Z + 50); task.wait(0.1)
+                    hrp.CFrame = CFrame.new(hrp.Position.X + 50, 100, hrp.Position.Z); task.wait(0.1)
+                    hrp.CFrame = CFrame.new(hrp.Position.X, 50, hrp.Position.Z); task.wait(0.1)
+                    hrp.CFrame = CFrame.new(hrp.Position.X - 50, 50, hrp.Position.Z); task.wait(0.1)
+                    hrp.CFrame = CFrame.new(hrp.Position.X, old_pos, hrp.Position.Z - 50); task.wait()
+                end); task.wait(); end
             end
         end
     end)
 
-    w2:Toggle("Invis explosion", false, function(e) 
-        invis_explosion_en = e
+    w2:Toggle("Invis explosion", false, function(bool) 
+        invis_explosion_en = bool
         workspace.ChildAdded:Connect(function(child)
             if child.Name == "Explosion" and invis_explosion_en then
                 child.BlastRadius = 0
@@ -98,25 +101,24 @@ print("Loading..")
         end)
     end)
 
-    w3:Toggle("Loop dash", false, function(e) 
-        dash_anim_en = e; if me.Character ~= nil and dash_anim_en then
+    w3:Toggle("Loop dash", false, function(bool) 
+        dash_anim_en = bool; if me.Character ~= nil and dash_anim_en then
             dash_anim = Instance.new("Animation")
             dash_anim.AnimationId = "rbxassetid://6237974108"
             dash_animer = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid"):FindFirstChild("Animator")
             dash_anim_track = dash_animer:LoadAnimation(dash_anim)
-            me.Character.Humanoid.WalkSpeed = 50
-            while dash_anim_en do
-                pcall(function()
-                    task.wait(0.2)
-                    dash_anim_track:Play()
-                end)
-            end
+            while dash_anim_en do pcall(function()
+                task.wait(0.2)
+                dash_anim_track:Play()
+                for _ = 1, 20 do
+                    local hrp, hum = me.Character.HumanoidRootPart, me.Character.Humanoid; hum.PlatformStand = false
+                    hrp.CFrame = hrp.CFrame * CFrame.new(0, 0, -1); rs.RenderStepped:Wait()
+                end
+            end); task.wait(); end
         else
             dash_anim_track:Stop()
             me.Character.Humanoid.WalkSpeed = 16
         end
     end)
-
-
 
 print("Loaded!")
